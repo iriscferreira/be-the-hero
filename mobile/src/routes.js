642c6c3be8f9d05d4844fc98c13c6c864d5cb0c0
Iris Ferreira <iris.ferreira@petronect.com.br>
2020-03-30 12:31:40 -0300

    insert incidentsList

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -9,9 +10,9 @@ import Detail from './pages/Detail';
 export default function Routes(){
     return(
         <NavigationContainer>
-            <AppStack.Navigator>
-                <AppStack.Screen component={Incidents} />
-                <AppStack.Screen component={Detail} />
+            <AppStack.Navigator screenOptions={ {headerShown: false }}>
+                <AppStack.Screen name="Incidents" component={Incidents} />
+                <AppStack.Screen name="Detail" component={Detail} />
             </AppStack.Navigator>
         </NavigationContainer>
     );
